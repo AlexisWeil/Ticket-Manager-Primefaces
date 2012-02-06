87b6cd25c2dcf729a22ad98b4f279ff6ac4598be
unknown <Alexis@.(none)>
2012-02-07 00:44:40 +0100

    Modifs pleins de trucs

---
+++ src/main/java/com/supinfo/ticketmanager/web/controller/ThemeController.java
@@ -0,0 +1,18 @@
+package com.supinfo.ticketmanager.web.controller;
+
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Alexis
+ * Date: 06/02/12
+ * Time: 22:14
+ * To change this template use File | Settings | File Templates.
+ */
+@Named
+@SessionScoped
+public class ThemeController implements Serializable {
+}
